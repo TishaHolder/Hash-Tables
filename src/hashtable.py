e2327467123e792c28778b59def2917f90e7b6d3
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2020-02-11 18:29:43 -0500

    3/4 tests pass

--- src/hashtable.py
+++ src/hashtable.py
@@ -1,3 +1,30 @@
+"""
+In computing, a hash table is a data structure that implements an associative array abstract data type, 
+a structure that can map keys to values. A hash table uses a hash function to compute an index, also called 
+a hash code, into an array of buckets or slots, from which the desired value can be found
+"""
+
+"""
+A hash map with a linked list separate chaining strategy follows a similar flow to a regular hash map. 
+The user wants to assign a value to a key in the map. The hash map takes the 
+key and transforms it into a hash code. The hash code is then converted into an index to an array using the 
+modulus operation. If the value of the array at the hash function’s returned index is empty, a new linked 
+list is created with the value as the first element of the linked list. If a linked list already exists at 
+the address, append the value to the linked list given.
+
+"""
+
+"""
+Hash tables are efficient key-value stores. They are capable of assigning and retrieving data in the fastest way 
+possible for a data structure. This is because the underlying data structure that they use is an array. 
+A value is stored at an array index determined by plugging the key into a hash function.
+
+****In Python we don’t have an array data structure that uses a contiguous block of memory. We are going 
+to simulate an array by creating a list and keeping track of the size of the list with an additional 
+integer variable. This will allow us to design something that resembles a hash table. 
+
+"""
+
 # '''
 # Linked List hash table key/value pair
 # '''
@@ -19,7 +46,9 @@ class HashTable:
         #this is where we will store all of the data
         self.storage = [None] * capacity 
 
-    #A hashing function takes a key and returns an index into the underlying array.
+    #uses python's built in hash function to convert the given key into any integer
+    #The underscore prefix is meant as a hint to another programmer that a variable or method starting with 
+    # a single underscore is intended for internal use.
     def _hash(self, key):
         '''
         Hash an arbitrary key and return an integer.
@@ -41,7 +70,7 @@ class HashTable:
 
     """
     Hashing functions return a wide range of integers. In order to transform these values into useful 
-    indices for our array we need a compression function. A compression function uses modular arithmetic 
+    indices for our list we need a compression function. A compression function uses modular arithmetic 
     to calculate an array index for a hash map when given a hash code.
     """
     def _hash_mod(self, key):
@@ -49,11 +78,12 @@ class HashTable:
         Take an arbitrary key and return a valid integer index
         within the storage capacity of the hash table.
         '''
-        #modulize the hash by the size of the linked list
+        #modulize the hash by the size of the list
+        #to get a number between 0 and the capacity of the list - 1
         return self._hash(key) % self.capacity
 
     """
-    Save the value to the map’s array at the index determined by plugging 
+    Save the value to the hash table’s list or array at the index determined by plugging 
     the key into the _hash() method and plugging the hash code into the _hash_mod() method.
     """
     def insert(self, key, value):
@@ -72,33 +102,40 @@ class HashTable:
         # After finding the list_index, we want to do a check of the content that’s currently at 
         # self.storage[list_index].
         # In order to avoid overwriting the wrong key, check the existing value in the list at 
-        # self.storage[list_index]. Save this into current_node.
-        current_node = self.storage[list_index]
+        # self.storage[list_index]. Save this into current_list_value.
+        current_list_value = self.storage[list_index]
 
-        #if there are no contents or current_node is equal to none, 
-        # we want to store the key and the value instead of just the key. 
-        # Instead of just saving value, save (key, value) to the list.
-        if current_node is None:
+        #if there are no contents or current_list_value is equal to none, 
+        #If the value of the list at the hash function’s returned index is empty, a new 
+        # linked list is created with the value as the first element of the linked list.
+        if current_list_value is None:
             self.storage[list_index] = LinkedPair(key, value)
             return
 
-        #If current_node_value already has contents, check if the saved key is different from the key 
-        # we are currently processing. If the keys are the same, overwrite the list value.
-        if current_node.key == key:
-            self.storage[list_index] = LinkedPair(key, value)
-            return        
+        #If current_list_value already has contents, check if the saved key is different from the key 
+        # we are currently processing. If the keys are the same, overwrite the list value.       
+        if current_list_value.key == key:
+            self.storage[list_index] = LinkedPair(key, value)            
+            return     
+                
+        #If a linked list already exists at the address, append the value to the linked list.
+        #also known as linked list chaining 
 
-        # If the keys are different or (current_node) holds a different key, 
-        # we’re going to implement linked list chaining        
-        while True:
-                #if the current node in the "inner linked list" is None, add the key, value pair
-                if current_node.next is None: 
-                    current_node.next = LinkedPair(key, value)
+        #set the current_list_value to current_node while traversing the inner linked list
+        #for readability sake
+        current_node = current_list_value
+
+        while current_node:
                 #if the current node key in the "inner linked list" is the same as the key we are
                 #currently processing, overwrite the node value
                 if current_node.key == key:
-                    current_node.next = LinkedPair(key, value)
+                    current_node = LinkedPair(key, value)
                     break
+                #if the next node in the "inner linked list" is None, append the value to the linked list
+                #and set the next pointer of the current node to the new node (LinkedPair)
+                if current_node.next is None: 
+                    current_node.next = LinkedPair(key, value)
+                #update the current_node pointer to the next node
                 current_node = current_node.next
 
 
@@ -110,7 +147,45 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+
+        #plug the key into _hash_mod to generate an index to insert the value
+        list_index = self._hash_mod(key)
+
+        # After finding the list_index, we want to retrieve the content that’s currently at 
+        # self.storage[list_index].        
+        current_list_value = self.storage[list_index]
+
+        #If the value of the list at the hash function’s returned index is empty, return None
+        if current_list_value is None:            
+            return current_list_value
+
+        #If the key at the hash function’s returned index matches the provided key
+        if current_list_value.key == key:
+            #place the value to be removed in a temp variable before the pointers are removed
+            removed = current_list_value
+            #removes the pointer at self.storage[list_index] to the next node before it is removed
+            self.storage[list_index] = current_list_value.next
+            #return the value for the given key
+            return removed.value
+        
+        #if the current_list_value is not None or doesn't match the key
+        #set the current_list_value.next to current_node while traversing the inner linked list
+        #for readability sake        
+        current_node = current_list_value.next
+        
+        while current_node is not None:
+            #if the current_node matches the key
+            if current_node.key == key:
+                #place the value to be removed in a temp variable before the pointers are removed
+                removed = current_node
+                #removes current node pointer to the next node before it is removed
+                current_node = current_node.next
+                #return the value of the removed node
+                return removed.value                    
+            
+            #set the current_node to the next node in the inner list      
+            current_node = current_node.next
+
 
     """
     calculate the array index in the same way insert does and then retrieve and return the value 
@@ -126,28 +201,26 @@ class HashTable:
         '''
         #plug the key into _hash_mod to generate an index to retrieve the value from
         list_index = self._hash_mod(self._hash(key))
-
+       
         #after finding the list index, we want to check to make sure that the index corresponds to the key 
-        # we’re looking for. Save the list value at our compressed hash code into possible_return_node.
-        possible_return_node = self.storage[list_index]
+        # we’re looking for. Save the list value at our compressed hash code into current_list_value.
+        current_list_value = self.storage[list_index]
 
         #Instead of just returning the array’s contents at that index, check if possible_return_value is None. 
         #If so, return None.
-        if possible_return_node is None:
+        if current_list_value is None:
             return None
         else:
-        #If possible_return_node is not None, check if the first element in possible_return_node 
-        # is the same as key. If so, return possible_return_node.value, the value.
-            while True:
-                if possible_return_node.key == key:
-                    return possible_return_node.value
-                possible_return_node = possible_return_node.next
-
-        # possible_return_value holds different key
-        print("ERROR")
-        return
-
-
+       
+            #if the current_list_value is not None or doesn't match the key
+            #set the current_list_value.next to current_node while traversing the inner linked list
+            #for readability sake        
+            current_node = current_list_value
+            
+            while current_node is not None:
+                if current_node.key == key:
+                    return current_node.value
+                current_node = current_node.next      
 
     def resize(self):
         '''
@@ -156,7 +229,21 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        old_storage = self.storage
+        self.capacity *= 2
+
+        self.storage = [None] * self.capacity
+
+        for item in old_storage:
+            
+            node = item
+
+            while node is not None:
+                self.insert(node.key, node.value)
+                node = node.next   
+               
+            
+
 
 
 if __name__ == "__main__":
